d7bdcc6887d7e659922d1ef9d54cd781af3bd57b
hamim2114 <hamim2114@gmail.com>
2025-10-25 21:23:01 +0600

    api endpoint added

--- src/utils/axiosReq.js
+++ src/utils/axiosReq.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL = window.location.hostname === 'localhost' 
   ? 'http://localhost:5000/api' 
-  : 'https://ahmed-server.vercel.app/api';
+  : 'https://ashiqur-server.vercel.app/api';
 
 export const axiosReq = axios.create({
   baseURL: baseURL,
